99f7ea4e2cb33d19a92e662c22eb29e886e708ae
Marius Petcu <dapetcu21@gmail.com>
2021-02-19 11:47:07 +0200

    Implement run in game

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,11 @@
 import { ExtensionContext } from "vscode";
 import { activateDiagnostics } from "./diagnostics";
+import { activateNotify } from "./notify";
 import { activateSyntax } from "./syntax";
 
 // Extension activation
 export function activate(context: ExtensionContext) {
   activateSyntax(context);
   activateDiagnostics(context);
+  activateNotify(context);
 }
---
+++ src/notify.ts
@@ -0,0 +1,41 @@
+import * as vscode from "vscode";
+import * as path from "path";
+import fetch from "node-fetch";
+
+async function notifyGame(endpoint) {
+  const url = `http://127.0.0.1:3648${endpoint}`;
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    if (data.error) {
+      vscode.window.showErrorMessage(data.error);
+    }
+  } catch (err) {
+    console.error(err);
+    vscode.window.showErrorMessage(
+      "Could not connect to the game. Make sure you are running it from Defold on this computer."
+    );
+  }
+}
+  
+export function activateNotify(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("fuior.runInGame", () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showErrorMessage("No fuior file is open");
+        return;
+      }
+
+      const data = JSON.stringify({
+        data: editor.document.getText(),
+        filename: path.basename(editor.document.uri.fsPath),
+      });
+
+      const base64Encoded = Buffer.from(data, "utf8").toString("base64");
+
+      notifyGame(`/load_fuior/${base64Encoded}`);
+    })
+  );
+}
